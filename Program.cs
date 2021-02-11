717e2eaa6b9cbb1f0be3d853f75a8a1b6605c0d1
TSG <TSG@TSGAC>
2021-02-11 10:51:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Practice: Creating arrays
+            string unitId01 = "OSC";
+            string unitId02 = "FLT";
+            string unitId03 = "FX1";
+            string unitId04 = "ADSR";
+
+            string[] units = new string[] {};
+
+
+            Console.WriteLine(unitId01);
+        }
+    }
+}
