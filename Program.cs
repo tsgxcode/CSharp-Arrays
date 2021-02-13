de9441665a120269adc69e0be712930be90a6e53
TSG <TSG@TSGAC>
2021-02-13 10:34:28 -0500

    Refactored to simplest code.
    2.13.2021

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace CSharp_Arrays
         static void Main(string[] args)
         {
             //Practice: Creating arrays
-            int[] synthModuals = new int[4];
-            synthModuals = new int[] { 2, 44, 5 };
+
+            int[] synthModuals = { 2, 44, 5 };
+
 
             Console.WriteLine(synthModuals);
         }
